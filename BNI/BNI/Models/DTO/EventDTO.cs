4d02cfbcccfafd77ec4e45ad35f4875868481b03
phu1110 <104994250+phu1110@users.noreply.github.com>
2024-03-22 10:09:15 +0700

    Database (#8)

---
+++ BNI/BNI/Models/DTO/EventDTO.cs
@@ -0,0 +1,39 @@
+﻿using BNI.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BNI.Models.DTO
+{
+    public class EventDTO
+    {
+        public string Title { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Address { get; set; }
+        public string OrganizerName { get; set; }
+        public string OrganizerEmail { get; set; }
+        [FromForm(Name = "Titles")]
+        public List<TitleDTO> Titles { get; set; }
+    }
+    
+    public class TitleDTO
+    {
+        public string Title { get; set; }
+        public List<string> ImagePaths { get; set; }
+    }
+    public class AddEventDTO
+    {
+        public string Title { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Address { get; set; }
+        public string OrganizerName { get; set; }
+        public string OrganizerEmail { get; set; }
+    }
+    
+    public class AddTitleDTO
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public List<IFormFile> ImagePaths { get; set; }
+    }
+}
